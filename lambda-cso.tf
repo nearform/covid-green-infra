16a1c7d3714235c4c5f3e7f571b605ccaadb5934
pmcgrath <pmcgrath@gmail.com>
2020-08-21 10:38:51 +0100

    Fix

--- lambda-cso.tf
+++ lambda-cso.tf
@@ -83,7 +83,7 @@ resource "aws_lambda_function" "cso" {
   filename         = local.lambdas_using_s3_as_source ? null : "${path.module}/.zip/${module.labels.id}_cso.zip"
   s3_bucket        = local.lambdas_using_s3_as_source ? var.lambdas_custom_s3_bucket : null
   s3_key           = local.lambdas_using_s3_as_source ? var.lambda_cso_s3_key : null
-  source_code_hash = local.lambdas_using_s3_as_source ? "" : data.archive_file.cso[0].output_base64sha256
+  source_code_hash = local.lambdas_using_s3_as_source ? "" : data.archive_file.cso.output_base64sha256
 
   function_name = "${module.labels.id}-cso"
   handler       = "cso.handler"
