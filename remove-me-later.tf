57f453780489437f59bc0ffa5d5c9ad7fa225c34
pmcgrath <pmcgrath@gmail.com>
2020-09-16 18:14:41 +0100

    Restored passthrough value

--- remove-me-later.tf
+++ remove-me-later.tf
@@ -93,7 +93,7 @@ resource "aws_api_gateway_integration" "api_healthcheck_head_integration" {
   rest_api_id          = aws_api_gateway_rest_api.main.id
   resource_id          = aws_api_gateway_resource.api_healthcheck.id
   http_method          = aws_api_gateway_method.api_healthcheck_head.http_method
-  passthrough_behavior = "NEVER"   # PENDING:  WHEN_NO_TEMPLATES"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
   type                 = "MOCK"
   request_templates = {
     "application/json" = jsonencode({
