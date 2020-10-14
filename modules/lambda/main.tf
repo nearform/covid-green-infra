59c7d868ac7bf2144f3d60aa64ce35f1e0b1a2d9
pmcgrath <pmcgrath@gmail.com>
2020-10-14 20:33:46 +0100

    Add link comment

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -216,6 +216,7 @@ data "aws_iam_policy_document" "this" {
     }
   }
 
+  # See https://docs.aws.amazon.com/IAM/latest/UserGuide/list_amazonses.html
   # See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/control-user-access.html
   dynamic statement {
     for_each = length(var.ses_send_emails_from_email_addresses) > 0 ? { 1 : 1 } : {}
